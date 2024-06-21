3218680bbb5e10fa617bd136b69b5494c3f665b3
kuotsanhsu <learningstud@gmail.com>
2024-06-21 12:24:21 +0800

    quantifiers_and_equality: rename md to lean

--- quantifiers_and_equality.lean
+++ quantifiers_and_equality.lean
@@ -1,9 +1,11 @@
+/-
 What follows are some common identities involving the existential
 quantifier. In the exercises below, we encourage you to prove as many
 as you can. We also leave it to you to determine which are
 nonconstructive, and hence require some form of classical reasoning.
+-/
 
-```lean
+section
 open Classical
 
 variable (α : Type) (p q : α → Prop)
@@ -22,45 +24,52 @@ example : (¬ ∀ x, p x) ↔ (∃ x, ¬ p x) := sorry
 example : (∀ x, p x → r) ↔ (∃ x, p x) → r := sorry
 example (a : α) : (∃ x, p x → r) ↔ (∀ x, p x) → r := sorry
 example (a : α) : (∃ x, r → p x) ↔ (r → ∃ x, p x) := sorry
-```
+end
 
+/-
 1. Prove these equivalences:
+-/
 
-```lean
+section
 variable (α : Type) (p q : α → Prop)
 
 example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) := sorry
 example : (∀ x, p x → q x) → (∀ x, p x) → (∀ x, q x) := sorry
 example : (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x := sorry
-```
+end
 
+/-
 You should also try to understand why the reverse implication is not derivable in the last example.
 
 2. It is often possible to bring a component of a formula outside a
    universal quantifier, when it does not depend on the quantified
    variable. Try proving these (one direction of the second of these
    requires classical logic):
+-/
 
-```lean
+section
 variable (α : Type) (p q : α → Prop)
 variable (r : Prop)
 
 example : α → ((∀ x : α, r) ↔ r) := sorry
 example : (∀ x, p x ∨ r) ↔ (∀ x, p x) ∨ r := sorry
 example : (∀ x, r → p x) ↔ (r → ∀ x, p x) := sorry
-```
+end
 
+/-
 3. Consider the "barber paradox," that is, the claim that in a certain
    town there is a (male) barber that shaves all and only the men who
    do not shave themselves. Prove that this is a contradiction:
+-/
 
-```lean
+section
 variable (men : Type) (barber : men)
 variable (shaves : men → men → Prop)
 
 example (h : ∀ x : men, shaves barber x ↔ ¬ shaves x x) : False := sorry
-```
+end
 
+/-
 4. Remember that, without any parameters, an expression of type
    ``Prop`` is just an assertion. Fill in the definitions of ``prime``
    and ``Fermat_prime`` below, and construct each of the given
@@ -70,8 +79,9 @@ example (h : ∀ x : men, shaves barber x ↔ ¬ shaves x x) : False := sorry
    that every odd number greater than 5 is the sum of three
    primes. Look up the definition of a Fermat prime or any of the
    other statements, if necessary.
+-/
 
-```lean
+section
 def even (n : Nat) : Prop := sorry
 
 def prime (n : Nat) : Prop := sorry
@@ -87,7 +97,9 @@ def goldbach_conjecture : Prop := sorry
 def Goldbach's_weak_conjecture : Prop := sorry
 
 def Fermat's_last_theorem : Prop := sorry
-```
+end
 
+/-
 5. Prove as many of the identities listed in the Existential
    Quantifier section as you can.
+-/
