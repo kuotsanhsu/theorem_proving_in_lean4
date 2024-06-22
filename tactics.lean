ffac5a90bdd8c3913413d78f129979d488f55d3e
kuotsanhsu <learningstud@gmail.com>
2024-06-22 16:55:34 +0800

    tactics: rename md to lean

--- tactics.lean
+++ tactics.lean
@@ -1,13 +1,15 @@
+/-
 1. Go back to the exercises in [Chapter Propositions and
 Proofs](./propositions_and_proofs.md) and
 [Chapter Quantifiers and Equality](./quantifiers_and_equality.md) and
 redo as many as you can now with tactic proofs, using also ``rw``
 and ``simp`` as appropriate.
+-/
 
+/-
 2. Use tactic combinators to obtain a one line proof of the following:
+-/
 
-```lean
 example (p q r : Prop) (hp : p)
         : (p ∨ q ∨ r) ∧ (q ∨ p ∨ r) ∧ (q ∨ r ∨ p) := by
   admit
-```
