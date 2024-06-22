c4c2cc2181823d1b26574ad58d3139716c05f89a
kuotsanhsu <learningstud@gmail.com>
2024-06-22 17:53:04 +0800

    tactics: copy exercises from propositions_and_proofs and quantifiers_and_equality

--- tactics.lean
+++ tactics.lean
@@ -6,6 +6,146 @@ redo as many as you can now with tactic proofs, using also ``rw``
 and ``simp`` as appropriate.
 -/
 
+section propositions_and_proofs
+section
+variable (p q r : Prop)
+
+-- commutativity of ∧ and ∨
+example : p ∧ q ↔ q ∧ p := by
+  admit
+example : p ∨ q ↔ q ∨ p := by
+  admit
+
+-- associativity of ∧ and ∨
+example : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r) := by
+  admit
+example : (p ∨ q) ∨ r ↔ p ∨ (q ∨ r) := by
+  admit
+
+-- distributivity
+example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) := by
+  admit
+example : p ∨ (q ∧ r) ↔ (p ∨ q) ∧ (p ∨ r) := by
+  admit
+
+-- other properties
+example : (p → (q → r)) ↔ (p ∧ q → r) := by
+  admit
+example : ((p ∨ q) → r) ↔ (p → r) ∧ (q → r) := by
+  admit
+example : ¬(p ∨ q) ↔ ¬p ∧ ¬q := by
+  admit
+example : ¬p ∨ ¬q → ¬(p ∧ q) := by
+  admit
+example : ¬(p ∧ ¬p) := by
+  admit
+example : p ∧ ¬q → ¬(p → q) := by
+  admit
+example : ¬p → (p → q) := by
+  admit
+example : (¬p ∨ q) → (p → q) := by
+  admit
+example : p ∨ False ↔ p := by
+  admit
+example : p ∧ False ↔ False := by
+  admit
+example : (p → q) → (¬q → ¬p) := by
+  admit
+end
+
+section
+open Classical
+
+variable (p q r : Prop)
+
+example : (p → q ∨ r) → ((p → q) ∨ (p → r)) := by
+  admit
+example : ¬(p ∧ q) → ¬p ∨ ¬q := by
+  admit
+example : ¬(p → q) → p ∧ ¬q := by
+  admit
+example : (p → q) → (¬p ∨ q) := by
+  admit
+example : (¬q → ¬p) → (p → q) := by
+  admit
+example : p ∨ ¬p := by
+  admit
+example : (((p → q) → p) → p) := by
+  admit
+end
+
+/-
+Prove ``¬(p ↔ ¬p)`` without using classical logic.
+-/
+example : ¬(p ↔ ¬p) := by
+  admit
+end propositions_and_proofs
+
+section quantifiers_and_equality
+section
+open Classical
+
+variable (α : Type) (p q : α → Prop)
+variable (r : Prop)
+
+example : (∃ x : α, r) → r := by
+  admit
+example (a : α) : r → (∃ x : α, r) := by
+  admit
+example : (∃ x, p x ∧ r) ↔ (∃ x, p x) ∧ r := by
+  admit
+example : (∃ x, p x ∨ q x) ↔ (∃ x, p x) ∨ (∃ x, q x) := by
+  admit
+
+example : (∀ x, p x) ↔ ¬ (∃ x, ¬ p x) := by
+  admit
+example : (∃ x, p x) ↔ ¬ (∀ x, ¬ p x) := by
+  admit
+example : (¬ ∃ x, p x) ↔ (∀ x, ¬ p x) := by
+  admit
+example : (¬ ∀ x, p x) ↔ (∃ x, ¬ p x) := by
+  admit
+
+example : (∀ x, p x → r) ↔ (∃ x, p x) → r := by
+  admit
+example (a : α) : (∃ x, p x → r) ↔ (∀ x, p x) → r := by
+  admit
+example (a : α) : (∃ x, r → p x) ↔ (r → ∃ x, p x) := by
+  admit
+end
+
+section
+variable (α : Type) (p q : α → Prop)
+
+example : (∀ x, p x ∧ q x) ↔ (∀ x, p x) ∧ (∀ x, q x) := by
+  admit
+example : (∀ x, p x → q x) → (∀ x, p x) → (∀ x, q x) := by
+  admit
+example : (∀ x, p x) ∨ (∀ x, q x) → ∀ x, p x ∨ q x := by
+  admit
+end
+
+section
+variable (α : Type) (p q : α → Prop)
+variable (r : Prop)
+
+example : α → ((∀ x : α, r) ↔ r) := by
+  admit
+example : (∀ x, p x ∨ r) ↔ (∀ x, p x) ∨ r := by
+  admit
+example : (∀ x, r → p x) ↔ (r → ∀ x, p x) := by
+  admit
+end
+
+section
+variable (men : Type) (barber : men)
+variable (shaves : men → men → Prop)
+
+example (h : ∀ x : men, shaves barber x ↔ ¬ shaves x x) : False := by
+  admit
+end
+end quantifiers_and_equality
+
 /-
 2. Use tactic combinators to obtain a one line proof of the following:
 -/
